abc253853d821f65e9f656bf458fadf7c42de566
bifft2 <bifft95@gmail.com>
2021-01-25 21:06:47 -0600

    added variables & constants notes

--- Notes/1-26.playground/Contents.swift
+++ Notes/1-26.playground/Contents.swift
@@ -1,3 +1,45 @@
-import UIKit
+// VARIABLES & CONSTANTS (and comments)
 
-var str = "Hello, playground"
+
+// An immutable object CANNOT be updated or changed
+// constants (starting with the word let) are immutable
+
+
+// a mutable object CAN be updated or changed
+// variables are mutable
+
+
+/*
+ Why we use constants:
+ 1. the compiler knows it should never be changed, and will not let you build or run your code if it is changed somwhere within your program
+ 2. it takes less space and makes our code more efficient
+ 3. it's the swift way
+ */
+
+
+/* TODO:
+ Imagine you're calculating information about the distance traveled on a trip.
+ The program can be reused, but it tracks only one trip at a time.
+ Would you represent the following values with constants or variables? What might they look like?
+ - starting location
+ - destination
+ - current location
+ - distance traveled
+ - remaining distance
+ */
+
+
+// VARIABLE NAMING
+
+/*
+You must follow:
+ 1. no mathematical symbols
+ 2. no spaces
+ 3. cannot begin with a number
+ */
+
+/*
+You should follow:
+ 1. clear and descriptive
+ 2. camel case
+ */
